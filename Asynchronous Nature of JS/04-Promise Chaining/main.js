4c41d44b4e95c81544004434ad96111370ba74a1
Vishnu Vardhan Munnangi <mvishnuvardhanvijay@gmail.com>
2026-01-12 18:30:49 +0530

    Added promise chaining

---
+++ Asynchronous Nature of JS/04-Promise Chaining/main.js
@@ -0,0 +1,37 @@
+function wait1(t) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Wait-1 is over");
+    }, t * 1000);
+  });
+}
+
+function wait2(t) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Wait-2 is over");
+    }, t * 1000);
+  });
+}
+
+function wait3(t) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Wait-3 is over");
+    }, t * 1000);
+  });
+}
+
+function main(t1, t2, t3) {
+  wait1(t1).then((data) => {
+    console.log(data);
+    return wait2();
+  }).then((data) => {
+    console.log(data);
+    return wait3();
+  }).then((data) => {
+    console.log(data);
+  });
+}
+
+main();
