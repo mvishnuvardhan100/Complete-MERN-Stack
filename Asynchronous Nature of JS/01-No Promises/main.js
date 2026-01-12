c6169a98fe8db87f22a85bfac6b91c2e1989a303
Vishnu Vardhan Munnangi <mvishnuvardhanvijay@gmail.com>
2026-01-12 11:28:14 +0530

    Added how to use Async Function setTimeout

---
+++ Asynchronous Nature of JS/01-No Promises/main.js
@@ -0,0 +1,18 @@
+function displayName() {
+  console.log("Name: M Vijay Vishnu Vardhan");
+}
+
+function displayAge() {
+  console.log("Age: " + 19);
+}
+
+setTimeout(displayName, 1000);
+displayAge();
+
+/*
+
+output:
+Age: 19
+Name: M Vijay Vishnu Vardhan
+
+*/
