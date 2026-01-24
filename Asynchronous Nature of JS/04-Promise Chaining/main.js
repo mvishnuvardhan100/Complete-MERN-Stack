e65b9693205e2176361758ee793c6037875fab2e
Vishnu Vardhan Munnangi <mvishnuvardhanvijay@gmail.com>
2026-01-24 18:17:35 +0530

    Modified Promise Chaining

--- Asynchronous Nature of JS/04-Promise Chaining/main.js
+++ Asynchronous Nature of JS/04-Promise Chaining/main.js
@@ -1,7 +1,7 @@
 function wait1(t) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve("Wait-1 is over");
+      resolve("Wait-1 completed");
     }, t * 1000);
   });
 }
@@ -9,7 +9,7 @@ function wait1(t) {
 function wait2(t) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve("Wait-2 is over");
+      resolve("Wait-2 completed");
     }, t * 1000);
   });
 }
@@ -17,21 +17,19 @@ function wait2(t) {
 function wait3(t) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve("Wait-3 is over");
+      resolve("Wait-3 completed");
     }, t * 1000);
   });
 }
 
-function main(t1, t2, t3) {
-  wait1(t1).then((data) => {
-    console.log(data);
-    return wait2();
-  }).then((data) => {
-    console.log(data);
-    return wait3();
-  }).then((data) => {
-    console.log(data);
-  });
-}
 
-main(1, 0.5, 1);
+wait1(1).then((data) => {
+  console.log(data);
+  return wait2(1)
+}).then((data) => {
+  console.log(data);
+  return wait3(1);
+}).then((data) => {
+  console.log(data);
+});
+
