68b36ec6c4fa9c784eb67fab6734dfafed1cac1a
Vishnu Vardhan Munnangi <mvishnuvardhanvijay@gmail.com>
2026-01-24 15:57:06 +0530

    Modified Promises without async & await

--- Asynchronous Nature of JS/02-Promises without async & await/main.js
+++ Asynchronous Nature of JS/02-Promises without async & await/main.js
@@ -1,25 +1,21 @@
-const fs = require("fs");
 const path = require("path");
-
-
+const fs = require("fs");
 const filePath = path.join(__dirname, "intro.txt");
-function vishnusFileRead() {
+
+function VishnusFileRead() {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, "utf-8", (err, data) => {
       if(err) {
         reject(err);
-      }
-      else {
+      } else {
         resolve(data);
       }
     });
   });
 }
 
-vishnusFileRead().then((data) => {
+VishnusFileRead().then((data) => {
   console.log(data);
-}).catch((err) => {
-  console.log(err);
 });
 
-console.log("Synchronous code ends");
+console.log("Synchronous code ended");
