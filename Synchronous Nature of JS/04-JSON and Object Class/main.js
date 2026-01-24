2eace7dba995a35fa38580b68271b134d58c8976
Vishnu Vardhan Munnangi <mvishnuvardhanvijay@gmail.com>
2026-01-24 14:49:22 +0530

    Modified JSON and Object Class

--- Synchronous Nature of JS/04-JSON and Object Class/main.js
+++ Synchronous Nature of JS/04-JSON and Object Class/main.js
@@ -1,27 +1,29 @@
-//JSON Class
-const userDetails = {
-  username: "mvishnuvardhan100",
-  following: 616,
-  followers: 211
+const userDetails1JSO = {
+  fullName: "Vijay",
+  age: 19,
+  isMarried: false,
+  achievements: [
+    "ICPC",
+    "Candidate Master",
+    "5 Star at CodeChef"
+  ]
 };
 
-let userDetailsJSON = JSON.stringify(userDetails);
-console.log(userDetailsJSON);
+const userDetails1JSON = JSON.stringify(userDetails1JSO);
+console.log(userDetails1JSON);
 
-let infoJSON = '{"firstName": "Vijay", "age" : 19}';
-const info = JSON.parse(infoJSON);
-console.log(info.firstName);
-console.log(info.age);
-
-//Object class
-console.log(Object.keys(userDetails));
-console.log(Object.values(userDetails));
-console.log(Object.entries(userDetails));
-Object.assign({}, userDetails, {checkmarkAvailable: false});
-if(!userDetails.checkmarkAvailable) {
-  console.log("Blue tick mark is not availabe");
+const userDetails2JSON = '{"fullName": "Rama", "age": 19, "isMarried": false}';
+const userDetails2JSO = JSON.parse(userDetails2JSON);
+console.log("Name: " + userDetails2JSO.fullName);
+console.log("Age: " + userDetails2JSO.age);
+if(userDetails2JSO.isMarried) {
+  console.log(userDetails2JSO.fullName + " is married");
+} else {
+  console.log(userDetails2JSO.fullName + " is not married");
 }
 
-
-//checking whether a particular key is present or not
-console.log(userDetails.hasOwnProperty("username"));
+//Object class
+console.log(Object.keys(userDetails1JSO));
+console.log(Object.values(userDetails1JSO));
+console.log(Object.entries(userDetails1JSO));
+console.log(userDetails1JSO.hasOwnProperty("fullName"));
